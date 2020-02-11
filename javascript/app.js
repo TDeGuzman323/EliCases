a7a80dc88290bd2d6de8c5b3d7040b2b9a14b331
Tara DeGuzman <tara.deguzman323@gmail.com>
2020-02-11 13:22:02 -0800

    modal function 1, image switch

--- javascript/app.js
+++ javascript/app.js
@@ -1,57 +1,24 @@
+// iPhone 11 Pro Modal //
+let img = document.querySelector ('#bamboo11ProMain');
+let bamboo = document.querySelector ('#bamboo11Pro'); 
+let cherry = document.querySelector ('#cherry11Pro');
+let rosewood = document.querySelector ('#rosewood11Pro');
+let walnut = document.querySelector ('#walnut11Pro');
 
-// --- product selection page, iphone --- //
 
-$('#staticBackdrop').on('show.bs.modal', function () {
-    $('.classFromElement').on('click', function (e) {
-        e.preventDefault();
-    });
+bamboo.addEventListener('click', () => {
+    img.src = "../Images/productCase/iPhone/iPhone11/bamboo_iPhone11PROproMax.png";
 })
 
-var alliPhonesImg = ["bamboo_iPhone11PROproMax","cherry_iPhone11PROproMax","rosewood_iPhone11PROproMax","walnut_iPhone11PROproMax"];
-var imgPro = document.querySelector(".imgPro img");
-var allCases = document.querySelectorAll(".allCases li");
+cherry.addEventListener('click', () => {
+    img.src = "../Images/productCase/iPhone/iPhone11/cherry_iPhone11PROproMax.png";
+})
 
-allCases.forEach(colors => colors.addEventListener("click", function (elem)  {
-    //--- remove class ACTIVE from code ---//
-    for (var i=0; i < allCases.length; i++) {
-        allCases[i].classList.remove("active");
-    }
+rosewood.addEventListener('click', () => {
+    img.src = "../Images/productCase/iPhone/iPhone11/rosewood_iPhone11PROproMax.png";
+})
 
-    function changeImage() {
-        
-    if (document.getElementById ("bamboo").src == "/Images/productCase/iPhone/iPhone11/bamboo_iPhone11PROproMax.png");
-    {
-        document.getElementById ("cherry").src = "/Images/productCase/iPhone/iPhone11/cherry_iPhone11PROproMax.png";
-    }
-        document.getElementById ("rosewood").src = "/Images/productCase/iPhone/iPhone11/rosewood_iPhone11PROproMax.png";
-    {
-        document.getElementById ("walnut").src = "/Images/productCase/iPhone/iPhone11/walnut_iPhone11PROproMax.png";
-    }
-    
-    }
-}))
-//     if (this.className === "bamboo"){
-//         imgPro.src = "../Images/productCase/iPhone/" +alliPhonesImg[0] +".png";
-//         imgPro.classList.add("animate");
-//     }
+walnut.addEventListener('click', () => {
+    img.src = "../Images/productCase/iPhone/iPhone11/walnut_iPhone11PROproMax.png";
+})
 
-//      if (this.className === "cherry"){
-//         imgPro.src = "../Images/productCase/iPhone/" +alliPhonesImg[1] +".png";
-//         imgPro.classList.add("animate");
-//     }
-
-//      if (this.className === "rosewood"){
-//         imgPro.src = "../Images/productCase/iPhone/" +alliPhonesImg[2] +".png";
-//         imgPro.classList.add("animate");
-//     }
-
-//      if (this.className === "walnut"){
-//         imgPro.src = "../Images/productCase/iPhone/" +alliPhonesImg[3] +".png";
-//         imgPro.classList.add("animate");
-//     }
-    
-//    setTimeout(function() {
-//        imgPro.classList.remove("animate");
-//    },500);
-
-// }))
