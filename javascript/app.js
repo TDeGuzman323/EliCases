0c8ea72b99f0dcbc6d2b3d6aeb8a5d19e663af7c
Tara DeGuzman <tara.deguzman323@gmail.com>
2020-02-19 01:53:00 -0800

    gallery

--- javascript/app.js
+++ javascript/app.js
@@ -1,40 +1,5 @@
-// variables //
 
-const cartBtn = document.querySelector(".cart-btn");
-const closeCartBtn = document.querySelector(".close-cart");
-const clearCartBtn = document.querySelector(".clear-cart");
-const cartDOM = document.querySelector(".cart");
-const cartOverlay = document.querySelector(".cart-overlay");
-const shopCartItems = document.querySelector (".shopcart-items");
-const cartTotal = document.querySelector(".cart-total");
-const cartContent = document.querySelector(".cart-content");
-const productsDOM = document.querySelector(".products-center");
+// --- GALLERY ZOOM --- //
 
 
-// Cart
-let cart = []
 
-// getting the products
-class Products {
-    getProducts(){
-        fetch()
-    }
-}
-
-//Display Products
-class UI {
-
-}
-
-
-// local storage
-class Storage {
-
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-const ui = new UI();
-
-const products = new Products();
-
-};)
