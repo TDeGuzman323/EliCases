1e1a3d167fc40795bcd4e731e644c48fd4cc90e7
Tara DeGuzman <tara.deguzman323@gmail.com>
2020-02-20 05:33:04 -0800

    about page moved, index tweaked

---
+++ javascript/app.js
@@ -0,0 +1,31 @@
+
+
+// ----- ABOUT PAGE, SCROLL EFFECT -----//
+
+(function($) {
+    $(document).ready(function() {
+        $.jScrollability([
+            {
+                'selector': '.text-wrapper',
+                'start': function($el) { return $el.offset().top + $el.height() },
+                'end': 'parent',
+                'fn': function($el,pcnt) {
+                    var $spans = $el.find('span');
+                    var point = Math.floor(($spans.length+1) * pcnt);
+                    $spans.each(function(i,el) {
+                        var $span = $(el);
+                        if (i < point) {
+                            $span.addClass('visible');
+                        } else {
+                            $span.removeClass('visible');
+                        }
+                    });
+                }
+            }
+        ]);
+    });
+})(jQuery);
+
+
+// ----- end of ABOUT PAGE, SCROLL EFFECT -----//
+
